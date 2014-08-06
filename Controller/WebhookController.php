b0840e90a528f8fced28151f624fd71b9e9c26e2
Alexandre Assouad <alexandre.assouad@gmail.com>
2014-08-06 15:28:01 +0000

    working on webhooks

--- Controller/WebhookController.php
+++ Controller/WebhookController.php
@@ -26,8 +26,8 @@ class WebhookController extends Controller
         $subscriptionAdapter = $this->get('kairos_subscription.subscription_adapter');
 
         if($request->isMethod('POST')) {
-            // in case
-            sleep(2);
+            // wait a little bit, can be usefull in some cases to be sure that subscription id has been saved
+            sleep(3);
             $webhookNotification = $subscriptionAdapter->parseWebhook($request);
 
             $subscriptions = $this->get('kairos_subscription.subscription.manager')->findBySubscriptionId($webhookNotification->subscription->id);
@@ -49,7 +49,20 @@ class WebhookController extends Controller
             $result = ob_get_clean();
 
             if(count($subscriptions) > 0) {
-                $result .= "Subscription id" . chr(13) . $subscriptions[0]->getId();
+
+                $subscription = $subscriptions[0];
+
+                $result .= "Subscription id" . chr(13) . $subscription->getId();
+
+                $eventName = $subscriptionAdapter->getSubscriptionEvent($subscription, $webhookNotification);
+
+                if($eventName) {
+                    $dispatcher = $this->get('event_dispatcher');
+                    $this->get('kairos_subscription.subscription.manager')->save($subscription);
+                    $event =  new SubscriptionEvent($subscription, $webhookNotification);
+                    $dispatcher->dispatch($eventName, $event);
+                    return new Response('ok', 200);
+                }
             }
 
             $result .= chr(13)."fin";
@@ -62,22 +75,10 @@ class WebhookController extends Controller
                 ->setBody($result);
             $this->get('mailer')->send($message);
 
-            // should send 200 instead of exception since the webhook will continue being sent.
-            if(count($subscriptions) == 0)
-                return new Response('nok', 200);
-                //throw new EntityNotFoundException('Could not find subscription entity');
 
-            $subscription = $subscriptions[0];
 
-            $eventName = $subscriptionAdapter->getSubscriptionEvent($subscription, $webhookNotification);
 
-            if($eventName) {
-                $dispatcher = $this->get('event_dispatcher');
-                $this->get('kairos_subscription.subscription.manager')->save($subscription);
-                $event =  new SubscriptionEvent($subscription, $webhookNotification);
-                $dispatcher->dispatch($eventName, $event);
-                return new Response('ok', 200);
-            }
+
 
             return new Response('nok', 400);
 
