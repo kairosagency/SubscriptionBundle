56fb0de807c050d138e692ef7a0bd298f30310ae
Alexandre Assouad <alexandre.assouad@gmail.com>
2014-08-19 16:36:28 +0200

    first test

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kairos\SubscriptionBundle\Tests\DependencyInjection;
+
+use Kairos\SubscriptionBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Yaml\Yaml;
+
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetConfigTreeBuilder()
+    {
+        $config = Yaml::parse(file_get_contents(__DIR__.'/ConfigFixtures/config.yml'));
+
+        $configuration = new Configuration();
+        $treeBuilder = $configuration->getConfigTreeBuilder();
+        $processor = new Processor;
+
+        $config = $processor->process($treeBuilder->buildTree(), $config);
+    }
+}
